b8d8b2ac49e3e35b14f9179d98347db1a198eb08
1 <margoloko@mail.ru>
2022-12-05 14:51:44 +0700

    add fbs

--- pomodoro.py
+++ pomodoro.py
@@ -1,5 +1,20 @@
 from tkinter import Tk, Entry, Label
-import pyautogui, sys
-from PyQt5.QtWidgets import QApplication, Qlabel
+from fbs_runtime.application_context.PyQt5 import ApplicationContext
+import sys
+from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow
+from PyQt5.QtCore import Qt
+
+
+if __name__=='__main__':
+    appctxt = ApplicationContext()
+
+
+    app = QApplication([])
+    lable = QLabel("Take a break!")
+
+    lable.show()
+
+    app.exec_()
+
 
 
