b3263202611b1d7b9e6771489ca8023ce6d5f1a1
1 <margoloko@mail.ru>
2022-12-06 11:31:18 +0700

    1

--- pomodoro.py
+++ pomodoro.py
@@ -1,20 +1,27 @@
-from tkinter import Tk, Entry, Label
-from fbs_runtime.application_context.PyQt5 import ApplicationContext
-import sys
+#from tkinter import Tk, Entry, Label
+
+from fbs_runtime.application_context.PyQT5 import ApplicationContext
 from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow
 from PyQt5.QtCore import Qt
+
+import sys
 
 
 if __name__=='__main__':
     appctxt = ApplicationContext()
+    window = QMainWindow()
+    window.resize(250, 150)
+    window.show()
+    exit_code = appctxt.app.exec__()
+    sys.exit(exit_code)
 
 
-    app = QApplication([])
-    lable = QLabel("Take a break!")
+    #app = QApplication([])
+    #lable = QLabel("Take a break!")
 
-    lable.show()
+    #lable.show()
 
-    app.exec_()
+    #app.exec_()
 
 
 
